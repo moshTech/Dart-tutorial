777b0f4602a55629e98ab77cf428956b7b5b1004
mosh <mosh.onaarajnr@gmail.com>
2022-02-27 12:01:24 +0100

    first commit

---
+++ bin/dart_application.dart
@@ -0,0 +1,33 @@
+void main(List<String> arguments) {
+  print(arguments);
+  print('Hello world!');
+
+  print("Afees");
+}
+
+
+// Single comment
+
+// () - Parenthensis
+// {} - Curly bracket/braces
+// [] - Square bracket
+//
+// fxgghggfc 
+
+// main() method/function
+
+// Multiline commemt
+
+/*
+ gfhfgihk
+ hgihljk;
+ hjghojl;
+ yjghijl
+ hfjh,hkjk
+ hjgjhj
+
+*/
+
+ //        ************ Variable ***************
+
+ 
